6f1b8f881da3f2353209bf740f989440174e0ac7
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2024-01-20 17:38:43 +0200

    Update createTables.sql

--- MySQL/Data Definition and Datatypes - Exercise/createTables.sql
+++ MySQL/Data Definition and Datatypes - Exercise/createTables.sql
@@ -33,11 +33,15 @@ ADD CONSTRAINT fk_town_id FOREIGN KEY (town_id) REFERENCES towns(id);
 
 -- 3
 
-INSERT INTO towns(id, name) VALUE (1, 'Sofia');
-INSERT INTO towns(id, name) VALUE (2, 'Plovdiv');
-INSERT INTO towns(id, name) VALUE (3, 'Varna');
+INSERT INTO towns(id, name)
+VALUE (1, 'Sofia'),
+(2, 'Plovdiv'),
+(3, 'Varna');
 
-INSERT INTO minions(id, name, age, town_id) VALUES (1, 'Kevin', 22, 1);
-INSERT INTO minions(id, name, age, town_id) VALUES (2, 'Bob', 15, 3);
-INSERT INTO minions(id, name, age, town_id) VALUES (3, 'Steward', NULL, 2);
+INSERT INTO minions(id, name, age, town_id)
+VALUES (1, 'Kevin', 22, 1),
+(2, 'Bob', 15, 3),
+(3, 'Steward', NULL, 2);
+
+
 
