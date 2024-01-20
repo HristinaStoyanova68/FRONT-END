0126abd665b25bf195a6f6f74f781b5d73ea2a5a
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2024-01-20 14:46:43 +0200

    Update createTables.sql

--- MySQL/Data Definition and Datatypes - Exercise/createTables.sql
+++ MySQL/Data Definition and Datatypes - Exercise/createTables.sql
@@ -14,3 +14,16 @@ CREATE TABLE towns(
     
     PRIMARY KEY (town_id)
 );
+
+ALTER TABLE towns
+DROP COLUMN town_id;
+
+ALTER TABLE towns
+ADD COLUMN id INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
+
+ALTER TABLE minions
+ADD COLUMN town_id INT;
+
+ALTER TABLE minions 
+ADD CONSTRAINT fk_town_id FOREIGN KEY (town_id) REFERENCES towns(id);
+
