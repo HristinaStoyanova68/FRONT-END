7ae3c8d240f5d431aa4fb1a61e98f76ea55f26bd
HRISTINA STOYANOVA <viara68@abv.bg>
2024-02-22 11:53:22 +0200

    start learning Angular

---
+++ Angular/01.Intro-to-Angular-and-TypeScript/ts-demo/app.js
@@ -0,0 +1,14 @@
+var a = 5;
+console.log(a * 2);
+var text = 'cats';
+console.log(text);
+var hasCats = true;
+console.log(hasCats);
+//number === Array<number>
+var numberArray = [1, 2, 3, 4];
+console.log(numberArray);
+var numberArray2 = [1, 2, 3, 4, 5];
+console.log(numberArray2);
+numberArray.forEach(function (num) {
+    console.log(num);
+});
---
+++ Angular/01.Intro-to-Angular-and-TypeScript/ts-demo/app.ts
@@ -0,0 +1,25 @@
+const a: number = 5;
+console.log(a * 2);
+
+const text: string = 'cats';
+console.log(text);
+
+const hasCats = true;
+console.log(hasCats);
+
+//number === Array<number>
+const numberArray: number[] = [1, 2, 3, 4];
+console.log(numberArray);
+
+const numberArray2: Array<number> = [1, 2, 3, 4, 5];
+console.log(numberArray2);
+
+numberArray.forEach((num) => {
+    console.log(num);
+    
+})
+
+
+
+
+
