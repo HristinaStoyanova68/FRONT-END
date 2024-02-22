83974e1cd97557208c6cb92e12505e9b1e7f15ba
HRISTINA STOYANOVA <viara68@abv.bg>
2024-02-22 20:42:37 +0200

    adding user-list component with some examples

--- Angular/02.Components/my-demo-app-16/src/app/user-list/user-list.component.ts
+++ Angular/02.Components/my-demo-app-16/src/app/user-list/user-list.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+type User = {
+    name: string;
+    age: number;
+}
+
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent {
-
+    users = [
+        {name: 'Pesho', age: 20},
+        {name: 'Ivan', age: 33},
+        {name: 'Petya', age: 34},
+        {name: 'Maya', age: 45},
+        {name: 'Nena', age: 66}
+    ] as User[];
 }
