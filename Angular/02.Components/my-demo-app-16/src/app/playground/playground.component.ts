fcebd9a0ff430abf493007afb3513e562ab2c676
HRISTINA STOYANOVA <viara68@abv.bg>
2024-02-23 18:26:39 +0200

    add Lifecycle Hooks

--- Angular/02.Components/my-demo-app-16/src/app/playground/playground.component.ts
+++ Angular/02.Components/my-demo-app-16/src/app/playground/playground.component.ts
@@ -1,23 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+
+const IMG_URL = 'https://images.unsplash.com/photo-1708458927308-f798522882a7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1M3x8fGVufDB8fHx8fA%3D%3D';
 
 @Component({
     selector: 'app-playground',
     templateUrl: './playground.component.html',
     styleUrls: ['./playground.component.css']
 })
-export class PlaygroundComponent {
+export class PlaygroundComponent implements OnInit, OnDestroy{
 
     greenBackground = 'background-green';
     isToggle = false;
 
-    imgUrl = 'https://images.unsplash.com/photo-1708458927308-f798522882a7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1M3x8fGVufDB8fHx8fA%3D%3D';
+    imgUrl = IMG_URL;
 
+    ngOnInit() {
+        // debugger;
+        console.log('Component Created!');
+    }
+
+    ngAfterViewInit() {
+        // debugger;
+        console.log('After Init!');
+    }
+
+    ngOnDestroy() {
+        //clean up of stuff
+        console.log('On Destroy!');
+    }
+
+    ngDoCheck() {
+        if (this.isToggle === true) {
+            console.log({isToggle: this.isToggle});
+            
+        }
+    }
 
     handleClick() {
         console.log('clicked');
 
         this.isToggle = !this.isToggle;
-        console.log(this.isToggle);
+        // console.log(this.isToggle);
     }
 
     //first way : getting just value
