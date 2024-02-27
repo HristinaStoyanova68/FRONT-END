5036980a6a3f05c8009417fe7547e30797f5d260
HRISTINA STOYANOVA <viara68@abv.bg>
2024-02-27 09:23:46 +0200

    Update data.ts

--- Angular/02.Components/Articles_Components_Lab/articles-demo/src/app/data/data.ts
+++ Angular/02.Components/Articles_Components_Lab/articles-demo/src/app/data/data.ts
@@ -6,16 +6,23 @@ export class ArticleData {
         let articles: Article[] = [];
 
         for (let i = 0; i < data.length; i++) {
-            const articleData: { title: string, description: string, author: string, imageUrl: string } = data[i];
-
-            articles[i] = new Article(
-                articleData.title,
-                articleData.description,
-                articleData.author,
-                articleData.imageUrl
-                );
+            articles[i] = {
+                title: data[i].title,
+                description: data[i].description,
+                author: data[i].author,
+                imageUrl: data[i].imageUrl
+            };
         }
 
         return articles;
     }
-}
+};
+
+// const articleData: { title: string, description: string, author: string, imageUrl: string } = data[i];
+
+//             articles[i] = new Article(
+//                 articleData.title,
+//                 articleData.description,
+//                 articleData.author,
+//                 articleData.imageUrl
+//                 );
