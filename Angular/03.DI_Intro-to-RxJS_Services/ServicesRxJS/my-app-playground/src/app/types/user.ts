37fb0133bc3f55b4f70ce8beef164104d8c2d023
HRISTINA STOYANOVA <viara68@abv.bg>
2024-02-27 14:56:08 +0200

    interface segregation - create User2

--- Angular/03.DI_Intro-to-RxJS_Services/ServicesRxJS/my-app-playground/src/app/types/user.ts
+++ Angular/03.DI_Intro-to-RxJS_Services/ServicesRxJS/my-app-playground/src/app/types/user.ts
@@ -2,3 +2,36 @@ export interface User {
     name: string;
     age: number;
 }
+
+export interface Geo {
+    lat: string;
+    lng: string;
+  };
+
+  export interface Address {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: Geo;
+  };
+
+  export interface Company {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+
+  export interface User2 {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    phone: string;
+  }
+
+export interface JsonPlaceholderUser extends User2 {
+        address: Address;
+        website: string;
+        company: Company;
+};
