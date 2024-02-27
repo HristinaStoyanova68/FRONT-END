b22ad821c277733dddeb718347a2136d99f2d68a
HRISTINA STOYANOVA <viara68@abv.bg>
2024-02-27 09:23:12 +0200

    Update app.component.ts

--- Angular/02.Components/Articles_Components_Lab/articles-demo/src/app/app.component.ts
+++ Angular/02.Components/Articles_Components_Lab/articles-demo/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Article } from './models/article.model';
+import { data } from './data/seed';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Article Site';
+
+  articles: Article[] = data;
 }
