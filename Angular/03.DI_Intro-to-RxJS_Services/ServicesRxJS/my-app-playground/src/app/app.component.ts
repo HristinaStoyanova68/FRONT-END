d4df18d3fe61cdaecaf29b4932bce331b4ea1e5f
HRISTINA STOYANOVA <viara68@abv.bg>
2024-02-27 11:29:56 +0200

    manually change detection demo

--- Angular/03.DI_Intro-to-RxJS_Services/ServicesRxJS/my-app-playground/src/app/app.component.ts
+++ Angular/03.DI_Intro-to-RxJS_Services/ServicesRxJS/my-app-playground/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app-playground';
+    title = 'my-app-playground';
+
+    constructor() {
+        setTimeout(() => {
+            this.title = 'Changed from Angular!';
+        }, 3000);
+    }
+
+
+    //when we manually change detection
+    // constructor(private cd: ChangeDetectorRef) {
+    //     setTimeout(() => {
+    //         this.title = 'Changed from Detector!';
+    //         this.cd.detectChanges();
+    //     }, 3000);
+    // }
 }
