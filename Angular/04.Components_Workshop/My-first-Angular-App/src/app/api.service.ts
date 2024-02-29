fbbedab7c047e46c9229d64cbc8e018fec5ddce7
HRISTINA STOYANOVA <viara68@abv.bg>
2024-03-01 01:30:44 +0200

    create environments

--- Angular/04.Components_Workshop/My-first-Angular-App/src/app/api.service.ts
+++ Angular/04.Components_Workshop/My-first-Angular-App/src/app/api.service.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,8 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClientModule) { }
+
+  getThemes( ) {}
+  getPosts() {}
 }
