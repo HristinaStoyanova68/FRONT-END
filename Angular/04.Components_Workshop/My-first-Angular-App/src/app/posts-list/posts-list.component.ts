9c7d84214749a0becec9f9574c5f3ad80fd1c074
HRISTINA STOYANOVA <viara68@abv.bg>
2024-03-01 13:15:20 +0200

    add postList logic

--- Angular/04.Components_Workshop/My-first-Angular-App/src/app/posts-list/posts-list.component.ts
+++ Angular/04.Components_Workshop/My-first-Angular-App/src/app/posts-list/posts-list.component.ts
@@ -1,21 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { Post } from '../types/post';
 
 @Component({
-  selector: 'app-posts-list',
-  templateUrl: './posts-list.component.html',
-  styleUrls: ['./posts-list.component.css']
+    selector: 'app-posts-list',
+    templateUrl: './posts-list.component.html',
+    styleUrls: ['./posts-list.component.css']
 })
 export class PostsListComponent implements OnInit {
+    postList: Post[] = [];
+    constructor(private apiService: ApiService) {
 
-  constructor(private apiService: ApiService) {
+    }
 
-  }
+    ngOnInit(): void {
+        this.apiService.getPosts(5).subscribe((posts) => {
+            console.log(posts[0]);
 
-  ngOnInit(): void {
-    this.apiService.getPosts(5).subscribe((posts) => {
-      console.log({ posts });
-
-    });
-  }
+            this.postList = posts;
+        });
+    }
 }
