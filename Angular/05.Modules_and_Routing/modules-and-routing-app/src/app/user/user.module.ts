0c78e3575074d170b860a59a78b9e0898a4943bd
HRISTINA STOYANOVA <viara68@abv.bg>
2024-03-11 12:06:02 +0200

    add user-detail.resolver and user-detail.guard logic

--- Angular/05.Modules_and_Routing/modules-and-routing-app/src/app/user/user.module.ts
+++ Angular/05.Modules_and_Routing/modules-and-routing-app/src/app/user/user.module.ts
@@ -4,26 +4,27 @@ import { UserListComponent } from './user-list/user-list.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { UserDetailComponent } from './user-detail/user-detail.component';
-import { UserResolve } from './user-detail/user-detail-resolver';
-
-
+import { UserResolve } from './user-detail/user-detail.resolver';
+import { AuthGuard } from './user-detail/user-detail.guard';
 
 @NgModule({
-  declarations: [
-    UserListComponent,
-    UserDetailComponent
-  ],
+  declarations: [UserListComponent, UserDetailComponent],
   imports: [
     CommonModule,
     SharedModule,
     RouterModule.forChild([
-      {path: 'user/list', component: UserListComponent},
-      {path: 'user/detail/:id', component: UserDetailComponent, resolve: {user: UserResolve}},
+      { path: 'user/list', component: UserListComponent },
+      {
+        path: 'user/detail/:id',
+        component: UserDetailComponent,
+        resolve: { user: UserResolve },
+        canActivate: [AuthGuard],
+      },
     ]),
   ],
   exports: [
     UserListComponent,
     // UserDetailComponent,
-  ]
+  ],
 })
-export class UserModule { }
+export class UserModule {}
