cc6100dd0bebd77fcd5033e8a99c373941539be1
HRISTINA STOYANOVA <viara68@abv.bg>
2024-03-31 01:18:59 +0200

    Update register.component.ts

--- Angular/07.Directives_and_Forms/directives-and-forms/src/app/register/register.component.ts
+++ Angular/07.Directives_and_Forms/directives-and-forms/src/app/register/register.component.ts
@@ -12,9 +12,34 @@ export class RegisterComponent {
     password: ['', [Validators.maxLength(4)]],
   });
 
+  ngAfterViewInit() {
+    if (this.registerForm) {
+      this.registerForm.valueChanges.subscribe((x) => {
+        console.log(x);
+        
+      })
+    }
+  }
+
   constructor(private fb: FormBuilder) {}
 
   handleSubmit(): void {
     console.log(this.registerForm.value);
+
+    const {email, password} = this.registerForm?.value;
+
+    if (this.registerForm?.invalid) {
+      console.log('RegisterForm is invalid!');
+
+      return;
+    }
+
+    console.log('invalid', this.registerForm.invalid);
+    
+    console.log(this.registerForm.value);
+
+    // Ways to clear form (reseting the data)
+    // form.reset();
+    this.registerForm.setValue({email: '', password: ''});
   }
 }
